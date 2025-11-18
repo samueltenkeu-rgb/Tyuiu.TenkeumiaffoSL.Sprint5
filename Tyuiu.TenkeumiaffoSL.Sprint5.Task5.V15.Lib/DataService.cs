a03def2fed6cc5289a26fe3245d8f0efd1a8825f
Миаффо Тенкеу Лоик Сэмюэль <samueltenkeu-rgb@outlook.com>
2025-11-18 23:57:57 +0500

    Task5

--- Tyuiu.TenkeumiaffoSL.Sprint5.Task5.V15.Lib/DataService.cs
+++ Tyuiu.TenkeumiaffoSL.Sprint5.Task5.V15.Lib/DataService.cs
@@ -1,38 +1,33 @@
 ﻿using System;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.TenkeumiaffoSL.Sprint5.Task5.V15.Lib
 {
-    public class DataService : ISprint5Task5V15
+    public class DataService : ISprint5Task5V16
     {
         public double LoadFromDataFile(string path)
         {
             if (!File.Exists(path))
                 throw new FileNotFoundException($"Файл не найден: {path}");
 
-            // Читаем все строки из файла
             string[] lines = File.ReadAllLines(path);
-
             double? minDivisibleBy5 = null;
 
             foreach (string line in lines)
             {
-                // Разделяем строку на числа по пробелам
                 string[] numbers = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
                 foreach (string numberStr in numbers)
                 {
                     try
                     {
-                        // Парсим число, заменяя точку на запятую для корректного парсинга
                         string normalizedNumber = numberStr.Replace(".", ",");
                         double number = double.Parse(normalizedNumber, CultureInfo.InvariantCulture);
 
-                        // Проверяем делимость на 5
-                        if (Math.Abs(number % 5) < 1e-10)
+                        // Только положительные числа, делящиеся на 5
+                        if (number > 0 && Math.Abs(number % 5) < 1e-10)
                         {
                             if (minDivisibleBy5 == null || number < minDivisibleBy5)
                                 minDivisibleBy5 = number;
@@ -40,15 +35,14 @@ namespace Tyuiu.TenkeumiaffoSL.Sprint5.Task5.V15.Lib
                     }
                     catch (FormatException)
                     {
-                        continue; // Пропускаем некорректные данные
+                        continue;
                     }
                 }
             }
 
             if (minDivisibleBy5 == null)
-                throw new Exception("В файле нет чисел, делящихся на 5");
+                throw new Exception("В файле нет положительных чисел, делящихся на 5");
 
-            // Округляем до 3 знаков после запятой
             return Math.Round(minDivisibleBy5.Value, 3);
         }
     }
