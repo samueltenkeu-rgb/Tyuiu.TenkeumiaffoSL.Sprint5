0c13a1003713924ae15fb028294bfa4de9d09946
Миаффо Тенкеу Лоик Сэмюэль <samueltenkeu-rgb@outlook.com>
2025-11-18 23:55:36 +0500

    Task5

---
+++ Tyuiu.TenkeumiaffoSL.Sprint5.Task5.V15.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Tyuiu.TenkeumiaffoSL.Sprint5.Task5.V15.Lib;
+
+namespace Tyuiu.TenkeumiaffoSL.Sprint5.Task5.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestLoadFromDataFile()
+        {
+            DataService ds = new DataService();
+
+            // Создаем тестовый файл с данными
+            string path = Path.Combine(Path.GetTempPath(), "TestFileTask5V15.txt");
+            File.WriteAllText(path, "12.5 3.2 15 7.0 25 8.5 5");
+
+            double result = ds.LoadFromDataFile(path);
+
+            // Числа, делящиеся на 5: 15, 25, 5, 12.5
+            // Минимальное из них: 5
+            double expected = 5.0;
+
+            Assert.AreEqual(expected, result);
+
+            File.Delete(path);
+        }
+    }
+}
---
+++ Tyuiu.TenkeumiaffoSL.Sprint5.Task5.V15/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using Tyuiu.TenkeumiaffoSL.Sprint5.Task5.V15.Lib;
+
+namespace Tyuiu.TenkeumiaffoSL.Sprint5.Task5.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Миаффо Тенкеу Лоик Сэмюэль | Вариант #15";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил: Миаффо Тенкеу Лоик Сэмюэль | ИСТНб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл, в котором есть набор значений. Найти минимальное вещественное*");
+            Console.WriteLine("* число в файле, которое делится на 5. Результат вывести на консоль.    *");
+            Console.WriteLine("* У вещественных значений округлить до трёх знаков после запятой.        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask5V15.txt";
+
+            Console.WriteLine($"* Путь к файлу: {path}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            try
+            {
+                DataService ds = new DataService();
+                double result = ds.LoadFromDataFile(path);
+
+                // Выводим содержимое файла для наглядности
+                if (File.Exists(path))
+                {
+                    Console.WriteLine("Содержимое файла:");
+                    string fileContent = File.ReadAllText(path);
+                    Console.WriteLine(fileContent);
+                    Console.WriteLine("-----------------------------------------------------------------------");
+                }
+
+                Console.WriteLine($"Минимальное вещественное число, делящееся на 5: {result}");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                Console.WriteLine("Убедитесь, что файл существует по пути: C:\\DataSprint5\\InPutDataFileTask5V15.txt");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.ReadKey();
+        }
+    }
+}
