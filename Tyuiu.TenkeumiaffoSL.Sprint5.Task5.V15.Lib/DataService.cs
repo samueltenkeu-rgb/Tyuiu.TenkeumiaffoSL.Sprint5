427927e24bb20862dba0deb54e0e024719e26ab3
Миаффо Тенкеу Лоик Сэмюэль <samueltenkeu-rgb@outlook.com>
2025-11-18 23:59:27 +0500

    Task5

--- Tyuiu.TenkeumiaffoSL.Sprint5.Task5.V15.Lib/DataService.cs
+++ Tyuiu.TenkeumiaffoSL.Sprint5.Task5.V15.Lib/DataService.cs
@@ -23,17 +23,17 @@ namespace Tyuiu.TenkeumiaffoSL.Sprint5.Task5.V15.Lib
                 {
                     try
                     {
-                        string normalizedNumber = numberStr.Replace(".", ",");
+                        string normalizedNumber = numberStr.Replace(",", "."); // корректно для парсинга
                         double number = double.Parse(normalizedNumber, CultureInfo.InvariantCulture);
 
-                        // Только положительные числа, делящиеся на 5
-                        if (number > 0 && Math.Abs(number % 5) < 1e-10)
+                        if (number > 0 && Math.Abs(number % 5) < 1e-6) // положительное и делится на 5
                         {
+                            // берем минимальное
                             if (minDivisibleBy5 == null || number < minDivisibleBy5)
                                 minDivisibleBy5 = number;
                         }
                     }
-                    catch (FormatException)
+                    catch
                     {
                         continue;
                     }
